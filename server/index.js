1822ebb1512ffae944552bd4be00b10b527423a1
DimasEka <76155589+masdimaseka@users.noreply.github.com>
2025-04-18 15:27:26 +0800

    add vercel config

--- server/index.js
+++ server/index.js
@@ -17,13 +17,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
+}
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
