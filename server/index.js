7cd3e8d4bd4e812985b39389174ac80280e9597b
DimasEka <76155589+masdimaseka@users.noreply.github.com>
2025-04-18 19:07:41 +0800

    add root api route

--- server/index.js
+++ server/index.js
@@ -30,7 +30,9 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => res.send("Server is Running! tes"));
+app.get("/", (req, res) => res.send("Server is Running!"));
+
+app.get("/api", (req, res) => res.send("API is Running!"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/penjahit", penjahitRoutes);
