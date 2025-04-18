4ac1efaf8549ce0bb61b81542cfeae9e386bfe7d
DimasEka <76155589+masdimaseka@users.noreply.github.com>
2025-04-19 00:55:33 +0800

    update cors

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://jalin-platform.vercel.app/",
+    origin: "https://jalin-platform.vercel.app",
     credentials: true,
   })
 );
