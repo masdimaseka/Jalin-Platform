920da44ef479abc2993f1570ecf0ac6d730bbe5d
DimasEka <76155589+masdimaseka@users.noreply.github.com>
2025-04-18 17:19:43 +0800

    update root api

--- server/index.js
+++ server/index.js
@@ -30,6 +30,8 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => res.send("Server is Running! tes"));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/penjahit", penjahitRoutes);
 app.use("/api/categories", categoryRoutes);
