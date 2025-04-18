ae0435fb73199bc6e41812d24107397a73d1c8f1
DimasEka <76155589+masdimaseka@users.noreply.github.com>
2025-04-19 00:03:14 +0800

    update cors

--- server/index.js
+++ server/index.js
@@ -17,15 +17,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE"],
-    })
-  );
-}
+app.use(
+  cors({
+    origin: "https://jalin-platform.vercel.app/",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
