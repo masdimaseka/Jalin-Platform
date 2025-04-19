6e9e0c6b6373a94daf1ba7e340d547aaa99b3fc4
DimasEka <76155589+masdimaseka@users.noreply.github.com>
2025-04-19 10:04:52 +0800

    fix cors

--- server/index.js
+++ server/index.js
@@ -17,10 +17,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-cors({
-  origin: "https://www.jalin.my.id",
-  credentials: true,
-});
+app.use(
+  cors({
+    origin: ["https://www.jalin.my.id", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
