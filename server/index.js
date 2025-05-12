1c4e2af002d3b4a9ac2db1ea08cb671a4a1bf2fd
DimasEka <76155589+masdimaseka@users.noreply.github.com>
2025-05-12 10:30:49 +0800

    add limit

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(
   })
 );
 
-// app.use(express.json({ limit: "5mb" }));
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("Server is Running!"));
