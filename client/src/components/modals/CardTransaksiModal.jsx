e10fb6eaf73e73d2e37900053b363f9c46e1457a
DimasEka <76155589+masdimaseka@users.noreply.github.com>
2025-05-31 11:08:09 +0800

    fixed modal

--- client/src/components/modals/CardTransaksiModal.jsx
+++ client/src/components/modals/CardTransaksiModal.jsx
@@ -13,69 +13,70 @@ export const CardTransaksiModal = ({ isOpen, onClose, transaksiData }) => {
         onClick={onClose}
       ></div>
       <dialog open className="modal z-50">
-        <Link
-          to={`/jahitan/${transaksiData._id}`}
-          className="card bg-base-100 w-80 shadow-sm cursor-pointer h-full hidden lg:block"
-        >
-          <button
-            onClick={onClose}
-            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-          >
-            ✕
-          </button>
-          <figure className="w-full h-48 overflow-hidden rounded-lg">
-            <img
-              src={transaksiData.image?.[0] || "/banner.png"}
-              alt="Jahitan"
-              className="w-full h-full object-cover"
-            />
-          </figure>
-          <div className="card-body flex justify-between">
-            <h2 className="card-title line-clamp-2">{transaksiData.judul}</h2>
-            <div className="flex flex-col lg:flex-row flex-wrap gap-4 mt-2">
-              <span className="flex items-center gap-1">
-                <Icon
-                  icon="material-symbols-light:work-history"
-                  width="20"
-                  height="20"
-                  className="text-primary-jalin"
-                />
-                <p className="text-sm">{transaksiData.status}</p>
-              </span>
-              <span className="flex items-center gap-1">
-                <Icon
-                  icon="solar:calendar-bold"
-                  width="16"
-                  height="16"
-                  color="red"
-                />
-                <p className="text-sm">
-                  {new Date(transaksiData.tenggatWaktu).toLocaleString(
-                    "id-ID",
-                    {
-                      day: "numeric",
-                      month: "long",
-                      year: "numeric",
-                      hour: "2-digit",
-                      minute: "2-digit",
-                      timeZone: "UTC",
-                    }
-                  )}
-                </p>
-              </span>
+        <div className="card bg-base-100 w-80 shadow-sm cursor-pointer ">
+          <form method="dialog">
+            <button
+              onClick={onClose}
+              className="btn btn-sm btn-circle bg-white btn-ghost absolute right-2 top-2"
+            >
+              ✕
+            </button>
+          </form>
+          <Link to={`/jahitan/${transaksiData._id}`}>
+            <figure className="w-full h-48 overflow-hidden rounded-lg">
+              <img
+                src={transaksiData.image?.[0] || "/banner.png"}
+                alt="Jahitan"
+                className="w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body flex justify-between">
+              <h2 className="card-title line-clamp-2">{transaksiData.judul}</h2>
+              <div className="flex flex-col lg:flex-row flex-wrap gap-4 mt-2">
+                <span className="flex items-center gap-1">
+                  <Icon
+                    icon="material-symbols-light:work-history"
+                    width="20"
+                    height="20"
+                    className="text-primary-jalin"
+                  />
+                  <p className="text-sm">{transaksiData.status}</p>
+                </span>
+                <span className="flex items-center gap-1">
+                  <Icon
+                    icon="solar:calendar-bold"
+                    width="16"
+                    height="16"
+                    color="red"
+                  />
+                  <p className="text-sm">
+                    {new Date(transaksiData.tenggatWaktu).toLocaleString(
+                      "id-ID",
+                      {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        timeZone: "UTC",
+                      }
+                    )}
+                  </p>
+                </span>
 
-              <span className="flex items-center gap-1">
-                <Icon
-                  icon="mage:delivery-truck-fill"
-                  width="20"
-                  height="20"
-                  className="text-primary-jalin"
-                />
-                <p className="text-sm">{transaksiData.pengerjaan}</p>
-              </span>
+                <span className="flex items-center gap-1">
+                  <Icon
+                    icon="mage:delivery-truck-fill"
+                    width="20"
+                    height="20"
+                    className="text-primary-jalin"
+                  />
+                  <p className="text-sm">{transaksiData.pengerjaan}</p>
+                </span>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+        </div>
       </dialog>
     </>
   );
